cb47f5c4235bf993ff1225649de7f1a1c99b23ca
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2021-03-02 17:24:26 +0100

    file for production settings

---
+++ settings/production.py
@@ -0,0 +1,20 @@
+from . import *
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = False
+ALLOWED_HOSTS = ['68.183.0.190']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("NAME_DB"),
+        'HOST':'localhost',
+        'PORT': 5432,
+        'USER': os.getenv("USER_DB"),
+        'PASSWORD': os.getenv("PASS_DB")
+    }
+}
