0bbe03aaf5ff5bfdb3de615c6de77e8b3fc51383
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2021-03-02 19:47:33 +0100

    new line to test

--- purbeurre/asgi.py
+++ purbeurre/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projet_10.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.local')
 
 application = get_asgi_application()
